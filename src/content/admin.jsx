173269b1ff12491068303dd9b5859986934dcf00
i3umbac <wendan@students.zcu.cz>
2025-04-29 00:42:04 +0200

    admin update

--- src/content/admin.jsx
+++ src/content/admin.jsx
@@ -1,15 +1,13 @@
 import MiniDrawer from "../components/adminNav.jsx"
 import AppRouter from "../components/adminRouter.jsx"
 
-const pages = ["Users","Branch verification","Branches","Warehouses","API","Company","Billing"];
-
 export default function Admin(){
     return (
         <>
             < MiniDrawer />
 
             <div className="container">
-                < AppRouter adminPage={ 'About' } />
+                < AppRouter adminPage={ 'franchise' } />
             </div>
         </>
         )
